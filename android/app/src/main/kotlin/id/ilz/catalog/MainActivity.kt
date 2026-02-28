eb8725524108289c4479c6f8848f375054d47cc4
helenoktaa <1123150205@global.ac.id>
2026-02-28 21:05:25 +0700

    add ChangeNotifierProvider in main

---
+++ android/app/src/main/kotlin/id/ilz/catalog/MainActivity.kt
@@ -0,0 +1,5 @@
+package id.ilz.catalog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    //membungkus aplikasi dengan ChangeNotifier Provider
+    //agar state bisa diakses dimana saja
+    ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: const MyApp()
+    )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
